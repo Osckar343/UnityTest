d4b707279639287a8f0c076afb127f7bd94ab791
alan lopez gonzalez <megahalo40@hotmail.com>
2023-03-26 23:58:43 -0600

    modificacion de movimiento

--- Assets/Test/Movement.cs
+++ Assets/Test/Movement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first frame update tengo sueño
     void Start()
     {
         
